9d4d791da00e4fa5d9cf99ba445abb04c9048bbb
Gabrielle Alcoforado <alcoforadogabrielle@gmail.com>
2026-06-12 10:12:05 -0300

    Descontologia

---
+++ aula_02_operadores/src/aula_02_operadores/OperadoresUnarios.java
@@ -0,0 +1,19 @@
+package aula_02_operadores;
+
+public class OperadoresUnarios {
+
+	public static void main(String[] args) {
+		
+		int contador = 1000;
+		
+		System.out.println("Valor inical de contador: " + contador);
+		
+		System.out.println("Valor Pré Incrementado: " + (++ contador));
+		
+		System.out.println("Vlor Pós Incremento: " + (contador ++));
+		
+		System.out.println("Novo Valor de contado: " + contador);
+
+	}
+
+}
