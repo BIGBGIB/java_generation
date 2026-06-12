d18281f14946d3cfb6f08b4cd6048bf006c0d75c
Gabrielle Alcoforado <alcoforadogabrielle@gmail.com>
2026-06-12 11:53:20 -0300

    SalárioeAbono

---
+++ aula_02_operadores/src/aula_02_operadores/SalarioeAbono.java
@@ -0,0 +1,25 @@
+package aula_02_operadores;
+
+import java.util.Scanner;
+
+public class SalarioeAbono {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		float salario, abono;
+		
+		System.out.print("Digite o salario: ");
+		salario = leia.nextFloat();
+		
+		System.out.print("Digite o abono: ");
+		abono = leia.nextFloat();
+		
+		System.out.printf("Salário Final: R$ %.2f", salario + abono);
+		
+
+	}
+
+}
+;
